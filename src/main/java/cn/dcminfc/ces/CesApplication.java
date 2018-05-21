804d9a3d93627696b81592df81dad4bb5e5b768d
cheninitial <cyl003@foxmail.com>
2018-05-21 23:35:03 +0800

    * Go2World

---
+++ src/main/java/cn/dcminfc/ces/CesApplication.java
@@ -0,0 +1,12 @@
+package cn.dcminfc.ces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesApplication.class, args);
+	}
+}
